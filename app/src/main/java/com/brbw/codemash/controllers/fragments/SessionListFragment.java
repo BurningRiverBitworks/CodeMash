9176889c7650891b7337e5209d41a60242007b96
David Shah <david.a.shah@gmail.com>
2015-01-02 15:49:32 -0500

    Empty session list message added

--- app/src/main/java/com/brbw/codemash/controllers/fragments/SessionListFragment.java
+++ app/src/main/java/com/brbw/codemash/controllers/fragments/SessionListFragment.java
@@ -3,8 +3,10 @@ package com.brbw.codemash.controllers.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
+import com.brbw.codemash.R;
 import com.brbw.codemash.models.Day;
 
 import java.util.ArrayList;
@@ -38,6 +40,13 @@ public class SessionListFragment extends ListFragment {
                 android.R.layout.simple_list_item_1,emptyList);
 
         setListAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        CharSequence message = getText(R.string.empty_session_list_message);
+        setEmptyText(message);
     }
 
     private boolean hasArguments() {
