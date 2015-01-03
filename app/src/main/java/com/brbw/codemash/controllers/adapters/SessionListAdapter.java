ee608ab76e6f388728176b48bb6eec64654264d0
David Shah <david.a.shah@gmail.com>
2015-01-03 12:36:52 -0500

    Favorites persisted through UserPreferences

--- app/src/main/java/com/brbw/codemash/controllers/adapters/SessionListAdapter.java
+++ app/src/main/java/com/brbw/codemash/controllers/adapters/SessionListAdapter.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import com.brbw.codemash.CodeMashApplication;
 import com.brbw.codemash.R;
 import com.brbw.codemash.models.Session;
+import com.brbw.codemash.models.UserPreferences;
 import com.brbw.codemash.util.ViewHelper;
 
 import java.util.List;
@@ -26,12 +31,32 @@ public class SessionListAdapter extends ArrayAdapter<Session> {
             convertView = ViewHelper.inflateForAdapter(getContext(), parent, LAYOUT_ID);
         }
 
-        Session session = getItem(position);
+        final Session session = getItem(position);
 
         ViewHelper viewHelper = new ViewHelper(convertView);
         viewHelper.setText(R.id.session_title, session.getTitle());
         viewHelper.loadImageFromUrlIntoImageView(R.id.speaker_image,
                 String.format("%s?s=100", session.getSessionImageUrl()));
+
+        CheckBox favorites = ViewHelper.findView(convertView, R.id.session_favorite);
+        favorites.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                UserPreferences userPreferences = CodeMashApplication.userPreferencesInstance();
+
+                if (isChecked != session.isFavorited()) {
+                    if (isChecked) {
+                        userPreferences.addFavorite(session);
+                    } else {
+                        userPreferences.removeFavorite(session);
+                    }
+                    session.isFavorited(isChecked);
+                }
+            }
+        });
+
+        favorites.setChecked(session.isFavorited());
 
         return convertView;
     }
