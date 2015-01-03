dff5d454e496f739d89d956d7499f1d512d1d19a
David Shah <david.a.shah@gmail.com>
2015-01-03 11:36:34 -0500

    Remove rendundant code

--- app/src/main/java/com/brbw/codemash/controllers/adapters/SessionListAdapter.java
+++ app/src/main/java/com/brbw/codemash/controllers/adapters/SessionListAdapter.java
@@ -4,12 +4,10 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 
 import com.brbw.codemash.R;
 import com.brbw.codemash.models.Session;
 import com.brbw.codemash.util.ViewHelper;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -34,21 +32,6 @@ public class SessionListAdapter extends ArrayAdapter<Session> {
         viewHelper.setText(R.id.session_title, session.getTitle());
         viewHelper.loadImageFromUrlIntoImageView(R.id.speaker_image,
                 String.format("%s?s=100", session.getSessionImageUrl()));
-
-
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.speaker_image);
-        String firstSpeakerUrl = session.getSpeakers().get(0).getGravatarUrl();
-
-        String imageUrl;
-        if (firstSpeakerUrl != null && !firstSpeakerUrl.startsWith("http")) {
-            imageUrl = String.format("http:%s?s=100", firstSpeakerUrl);
-        } else {
-            imageUrl = String.format("%s?s=100", firstSpeakerUrl);
-        }
-
-        Picasso.with(getContext())
-                .load(imageUrl)
-                .into(imageView);
 
         return convertView;
     }
