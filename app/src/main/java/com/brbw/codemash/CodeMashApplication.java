71a3644ff0e6d4cd59d6c16f9e2b81e980371711
David Shah <david.a.shah@gmail.com>
2015-01-03 12:31:42 -0500

    Introduce UserPreferences

--- app/src/main/java/com/brbw/codemash/CodeMashApplication.java
+++ app/src/main/java/com/brbw/codemash/CodeMashApplication.java
@@ -3,16 +3,28 @@ package com.brbw.codemash;
 import android.app.Application;
 
 import com.brbw.codemash.models.SessionList;
+import com.brbw.codemash.models.UserPreferences;
 import com.brbw.codemash.util.network.SimpleJsonRequester;
 
 public class CodeMashApplication extends Application {
 
     private static SessionList sessionList;
+    private static UserPreferences userPreferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        userPreferences = new UserPreferences(getBaseContext());
+    }
 
     public synchronized static SessionList sessionListInstance() {
         if (sessionList == null) {
             sessionList = new SessionList(new CodeMashService(new SimpleJsonRequester()));
         }
         return sessionList;
+    }
+
+    public synchronized static UserPreferences userPreferencesInstance() {
+        return userPreferences;
     }
 }
---
+++ app/src/main/java/com/brbw/codemash/models/UserPreferences.java
@@ -0,0 +1,41 @@
+package com.brbw.codemash.models;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserPreferences {
+
+    private static final String USER_PREFERENCES_KEY = "CodeMashPreferences2015";
+    private static final String FAVORITES = "FAVORITES";
+
+    private final Context context;
+
+    public UserPreferences(Context context) {
+        this.context = context;
+    }
+
+
+    public void addFavorite(Session currentSession) {
+        Set<String> favorites = getFavorites();
+        favorites.add(String.valueOf(currentSession.getId()));
+        getEditor().putStringSet(FAVORITES, favorites).apply();
+    }
+
+    public void removeFavorite(Session currentSession) {
+        Set<String> favorites = getFavorites();
+        favorites.remove(String.valueOf(currentSession.getId()));
+        getEditor().putStringSet(FAVORITES, favorites).apply();
+    }
+
+    private SharedPreferences.Editor getEditor() {
+        return context.getSharedPreferences(USER_PREFERENCES_KEY, Context.MODE_PRIVATE).edit();
+    }
+
+    private Set<String> getFavorites() {
+        SharedPreferences preferences = context.getSharedPreferences(USER_PREFERENCES_KEY, Context.MODE_PRIVATE);
+        return preferences.getStringSet(FAVORITES, new HashSet<String>());
+    }
+}
